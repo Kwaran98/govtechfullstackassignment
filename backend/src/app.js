550482bcaeee9a095f94986a379d1aa191576877
Kamalesh Waran <kwaranwork98@gmail.com>
2026-06-17 09:37:44 +0800

    Added GET Health check for deployment

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,15 +1,20 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
-const teacherRoutes = require('./routes/teacherRoutes');
-const classRoutes = require('./routes/classRoutes');
+const teacherRoutes = require("./routes/teacherRoutes");
+const classRoutes = require("./routes/classRoutes");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/api', teacherRoutes);
-app.use('/api', classRoutes);
+// A simple route to indicate API on Render:
+app.get("/", (req, res) => {
+  res.json({ status: "ok", message: "School Administration API" });
+});
+
+app.use("/api", teacherRoutes);
+app.use("/api", classRoutes);
 
 module.exports = app;
